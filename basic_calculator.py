63879b9b481618da00be6fd9520be6cf837b5d1a
John Carl V. Gailan <johncarlgailan@gmail.com>
2026-08-09 20:39:51 +0800

    Basic calculator V1

---
+++ basic_calculator.py
@@ -0,0 +1,16 @@
+first_number = int(input("Enter first number: "))
+second_number = int(input("Enter second number: "))
+operator = input("Enter the operator: ")
+
+if operator == "+":
+    print(first_number + second_number)
+elif operator == "-":
+    print(first_number - second_number)
+elif operator == "*":
+    print(first_number * second_number)
+elif operator == "/":     
+    print(first_number / second_number)
+else:
+    print("Invalid operator")
+
+
